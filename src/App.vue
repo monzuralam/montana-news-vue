bedaf137c9c79afa7b036729b6d4bbb2b2a65251
monzuralam <monzur707@gmail.com>
2023-08-17 13:04:56 +0600

    Footer Component has been added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import Header from "./components/Header/Header.Component.vue";
+import Footer from "./components/Footer/Footer.Component.vue";
 </script>
 
 <template>
   <Header />
+  <div class="container mx-auto">
+    <router-view></router-view>
+  </div>
+  <Footer />
 </template>
 
 <style scoped></style>
---
+++ src/components/Footer/Footer.Component.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer class="footer-section bg-gray-50 p-4">
+    <div class="container mx-auto">
+      <div class="flex">
+        <div class="flex-1">
+          <p class="text-base">
+            Copyright &copy; {{ year }} | Made with ❤️ in Bangladesh.
+          </p>
+        </div>
+        <div
+          class="flex flex-1 md:justify-end sm:justify-between align-middle gap-x-4"
+        >
+          <a href="">Facebook</a>
+          <a href="">Twiiter</a>
+          <a href="">Linkedin</a>
+          <a href="">Youtube</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
+  methods: {},
+};
+</script>
