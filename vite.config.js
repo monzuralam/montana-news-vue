3c80afe1625c46d9e30529f7cc92d9d72f4927b3
monzuralam <monzur707@gmail.com>
2023-08-19 11:47:02 +0600

    base url has been updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/montana-news-vue/',
+  base: 'https://monzuralam.github.io/montana-news-vue/',
   plugins: [vue()],
 })
