0ac6061370892b31e6c43bfd6ba4842c10e079ac
monzuralam <monzur707@gmail.com>
2023-08-20 09:22:07 +0600

    contact component has been updated

--- src/components/Contact/Contact.Component.vue
+++ src/components/Contact/Contact.Component.vue
@@ -1,8 +1,31 @@
 <template>
-  <h1>{{ title }}</h1>
+  <div class="contact relative py-6">
+    <h1 class="text-3xl font-light pb-3 mb-5 border-b border-gray-300">Contact Us</h1>
+    <div class="flex flex-wrap gap-8">
+      <div class="flex-1">
+        <h2 class="text-2xl font-light border-b pb-2">Head Office</h2>
+        <h3 class="text-xl font-light">Montana News</h3>
+        <p class="text-base font-light">Pirgachha, Rangpur, Bangladesh</p>
+        <p class="text-base font-light">Office: <a href="tel:+880-1512-345678">+880-1512-345678</a></p>
+        <p class="text-base font-light">Advertisement: <a href="tel:+880-1512-345678">+880-1512-345678</a></p>
+        <p class="text-base font-light">Fax: +880-1512-345678</p>
+        <p class="text-base font-light">Email: <a href="mailto:info@example.com">info@example.com</a></p>
+      </div>
+      <div class="flex-1">
+        <h2 class="text-2xl font-light border-b pb-2">Distict Office</h2>
+        <h3 class="text-xl font-light">Dhaka</h3>
+        <p class="text-base font-light">Khilkhet, Dhaka, Bangladesh</p>
+        <p class="text-base font-light">Office: <a href="tel:+880-1512-345678">+880-1512-345678</a></p>
+        <p class="text-base font-light">Advertisement: <a href="tel:+880-1512-345678">+880-1512-345678</a></p>
+        <p class="text-base font-light">Fax: +880-1512-345678</p>
+        <p class="text-base font-light">Email: <a href="mailto:info@example.com">info@example.com</a></p>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
+  name: "Contact",
   data() {
     return {
       title: "Contact",
