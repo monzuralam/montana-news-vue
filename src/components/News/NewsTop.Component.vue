f930a1a285b0b9d18026e3e6538ef71e4b5f69bd
monzuralam <monzur707@gmail.com>
2023-08-20 23:56:43 +0600

    NewsTop component design has been finished

---
+++ src/components/News/NewsTop.Component.vue
@@ -0,0 +1,279 @@
+<template>
+    <!-- latest section start here -->
+    <section class="relative my-5 pb-5 border-b">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-light mb-5">{{ category }}</h1>
+            <div class="grid md:grid-cols-4 grid-cols-1 gap-5">
+                <!-- news left start here -->
+                <div class="md:col-span-3">
+                    <div class="grid md:grid-cols-3 sm:grid-cols-1 gap-5">
+                        <!-- featured post item start here -->
+                        <div class="md:col-span-2 sm:col-auto">
+                            <div class="post relative">
+                                <a href="#" class="" target="_blank">
+                                    <div class="relative">
+                                        <div class="post-thumb">
+                                            <img
+                                                src="../../assets/images/posts/apple-app.jpg"
+                                                class="w-full object-cover"
+                                                alt="photo"
+                                            />
+                                        </div>
+                                        <div
+                                            class="absolute top-0 left-0 right-0 bg-gradient-to-t from-black h-full p-4"
+                                        >
+                                            <h2 class="text-2xl text-white">
+                                                Lorem ipsum dolor sit amet
+                                                consectetur adipisicing elit.
+                                                Perferendis, mollitia?
+                                            </h2>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <!-- featured post item stop here -->
+                        <!-- featured left post item start here -->
+                        <div class="">
+                            <a href="#">
+                                <div class="post relative">
+                                    <img
+                                        src="../../assets/images/posts/dengu.jpg"
+                                        class="w-full object-cover"
+                                        alt="thumbnail"
+                                    />
+                                    <h3
+                                        class="text-xl font-light leading-6 my-2"
+                                    >
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Cumque, beatae.
+                                    </h3>
+                                    <p class="text-base font-light mb-4">
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Magni iure quidem cum
+                                        quae, amet quisquam adipisci modi enim
+                                        ...
+                                    </p>
+                                    <p class="text-base font-light">
+                                        19 Minute Ago
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                        <!-- featured left post item stop here -->
+                        <!-- bottom post item start here -->
+                        <div class="">
+                            <a href="#">
+                                <div class="post relative">
+                                    <img
+                                        src="../../assets/images/posts/dengu.jpg"
+                                        class="w-full object-cover"
+                                        alt="thumbnail"
+                                    />
+                                    <h3
+                                        class="text-xl font-light leading-6 my-2"
+                                    >
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Cumque, beatae.
+                                    </h3>
+                                    <p class="text-base font-light mb-4">
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Magni iure quidem cum
+                                        quae, amet quisquam adipisci modi enim
+                                        ...
+                                    </p>
+                                    <p class="text-base font-light">
+                                        19 Minute Ago
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                        <!-- bottom post item stop here -->
+                        <!-- bottom post item start here -->
+                        <div class="">
+                            <a href="#">
+                                <div class="post relative">
+                                    <img
+                                        src="../../assets/images/posts/dengu.jpg"
+                                        class="w-full object-cover"
+                                        alt="thumbnail"
+                                    />
+                                    <h3
+                                        class="text-xl font-light leading-6 my-2"
+                                    >
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Cumque, beatae.
+                                    </h3>
+                                    <p class="text-base font-light mb-4">
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Magni iure quidem cum
+                                        quae, amet quisquam adipisci modi enim
+                                        ...
+                                    </p>
+                                    <p class="text-base font-light">
+                                        19 Minute Ago
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                        <!-- bottom post item stop here -->
+                        <!-- bottom post item start here -->
+                        <div class="">
+                            <a href="#">
+                                <div class="post relative">
+                                    <img
+                                        src="../../assets/images/posts/dengu.jpg"
+                                        class="w-full object-cover"
+                                        alt="thumbnail"
+                                    />
+                                    <h3
+                                        class="text-xl font-light leading-6 my-2"
+                                    >
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Cumque, beatae.
+                                    </h3>
+                                    <p class="text-base font-light mb-4">
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Magni iure quidem cum
+                                        quae, amet quisquam adipisci modi enim
+                                        ...
+                                    </p>
+                                    <p class="text-base font-light">
+                                        19 Minute Ago
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                        <!-- bottom post item stop here -->
+                    </div>
+                </div>
+                <!-- news left stop here -->
+                <!-- sidebar start here -->
+                <div class="relative">
+                    <!-- sidebar advertisement start here -->
+                    <div class="sidebar-ads pb-5 border-b">
+                        <a href="#" rel="nofollow" target="_blank">
+                            <img
+                                src="https://tpc.googlesyndication.com/simgad/10278602567541228790"
+                                class="w-full"
+                                alt="ads"
+                            />
+                        </a>
+                    </div>
+                    <!-- sidebar advertisement stop here -->
+                    <!-- selected news start here -->
+                    <div class="sidebar-selected-news py-5">
+                        <h2 class="text-xl font-light leading-5 border-b pb-5">
+                            Selected
+                        </h2>
+                        <div class="relative">
+                            <!-- selected news item start here -->
+                            <div
+                                class="selected-news-item border-b last:border-b-0 py-5"
+                            >
+                                <a href="#">
+                                    <div class="flex">
+                                        <div class="pr-5">
+                                            <span class="text-3xl font-light">
+                                                01
+                                            </span>
+                                        </div>
+                                        <div class="flex-1">
+                                            <h3 class="text-xl font-light">
+                                                Lorem ipsum, dolor sit amet
+                                                consectetur adipisicing elit.
+                                                Ipsa illo, consequuntur omnis
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <!-- selected news item stop here -->
+                            <!-- selected news item start here -->
+                            <div
+                                class="selected-news-item border-b last:border-b-0 py-5"
+                            >
+                                <a href="#">
+                                    <div class="flex">
+                                        <div class="pr-5">
+                                            <span class="text-3xl font-light">
+                                                02
+                                            </span>
+                                        </div>
+                                        <div class="flex-1">
+                                            <h3 class="text-xl font-light">
+                                                Lorem ipsum, dolor sit amet
+                                                consectetur adipisicing elit.
+                                                Ipsa illo, consequuntur omnis
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <!-- selected news item stop here -->
+                            <!-- selected news item start here -->
+                            <div
+                                class="selected-news-item border-b last:border-b-0 py-5"
+                            >
+                                <a href="#">
+                                    <div class="flex">
+                                        <div class="pr-5">
+                                            <span class="text-3xl font-light">
+                                                03
+                                            </span>
+                                        </div>
+                                        <div class="flex-1">
+                                            <h3 class="text-xl font-light">
+                                                Lorem ipsum, dolor sit amet
+                                                consectetur adipisicing elit.
+                                                Ipsa illo, consequuntur omnis
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <!-- selected news item stop here -->
+                            <!-- selected news item start here -->
+                            <div
+                                class="selected-news-item border-b last:border-b-0 py-5"
+                            >
+                                <a href="#">
+                                    <div class="flex">
+                                        <div class="pr-5">
+                                            <span class="text-3xl font-light">
+                                                04
+                                            </span>
+                                        </div>
+                                        <div class="flex-1">
+                                            <h3 class="text-xl font-light">
+                                                Lorem ipsum, dolor sit amet
+                                                consectetur adipisicing elit.
+                                                Ipsa illo, consequuntur omnis
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <!-- selected news item stop here -->
+                        </div>
+                    </div>
+                    <!-- selected news stop here -->
+                </div>
+                <!-- sidebar stop here -->
+            </div>
+        </div>
+    </section>
+    <!-- latest section stop here -->
+</template>
+<script>
+export default {
+    name: "NewsTop",
+    props: ["cat"],
+    data() {
+        return {
+            category: this.cat,
+        };
+    },
+    methods: {},
+};
+</script>
