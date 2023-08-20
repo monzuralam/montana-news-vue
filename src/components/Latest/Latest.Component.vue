ccf357527371334a3f3b60f97e1fc6124f5c53c3
monzuralam <monzur707@gmail.com>
2023-08-20 10:07:25 +0600

    latest route register & component has been added

---
+++ src/components/Latest/Latest.Component.vue
@@ -0,0 +1,10 @@
+<template>
+    <TopAdvertisement />
+</template>
+<script>
+import TopAdvertisement from "../Advertisement/TopAdvertisement.Component.vue";
+export default {
+    name: "Latest",
+    components: { TopAdvertisement },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,35 +1,48 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import "./style.css";
+import App from "./App.vue";
 import Home from "./components/Home/Home.Component.vue";
 import Blog from "./components/Blog/Blog.Component.vue";
 import Contact from "./components/Contact/Contact.Component.vue";
+import Latest from "./components/Latest/Latest.Component.vue";
 
 // vue-fontawesome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faFacebook, faXTwitter, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+    faFacebook,
+    faXTwitter,
+    faLinkedin,
+    faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 library.add(faFacebook, faXTwitter, faLinkedin, faYoutube);
 
 const routes = [
     {
         path: "/",
-        component: Home
+        component: Home,
     },
     {
         path: "/blog/",
-        component: Blog
+        component: Blog,
     },
     {
         path: "/contact/",
-        component: Contact
-    }
+        component: Contact,
+    },
+    {
+        path: "/latest/",
+        component: Latest,
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
+    routes,
 });
 
-createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App)
+    .use(router)
+    .component("font-awesome-icon", FontAwesomeIcon)
+    .mount("#app");
