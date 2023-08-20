8c9675b32b14700f0fee51aaa3591e4ec5c6d7ea
monzuralam <monzur707@gmail.com>
2023-08-20 23:57:05 +0600

    Newstop component has been added

--- src/components/Latest/Latest.Component.vue
+++ src/components/Latest/Latest.Component.vue
@@ -1,10 +1,12 @@
 <template>
     <TopAdvertisement />
+    <NewsTop cat="Latest" />
 </template>
 <script>
 import TopAdvertisement from "../Advertisement/TopAdvertisement.Component.vue";
+import NewsTop from "../News/NewsTop.Component.vue";
 export default {
     name: "Latest",
-    components: { TopAdvertisement },
+    components: { TopAdvertisement, NewsTop },
 };
 </script>
