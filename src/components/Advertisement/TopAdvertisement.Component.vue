aba12b591338bba96a890ec76d595623d696f382
monzuralam <monzur707@gmail.com>
2023-08-20 11:32:42 +0600

    border bottom has been added

--- src/components/Advertisement/TopAdvertisement.Component.vue
+++ src/components/Advertisement/TopAdvertisement.Component.vue
@@ -1,23 +1,23 @@
 <template>
-  <section class="relative overflow-hidden py-5">
-    <div class="container mx-auto">
-      <a :href="topAdvertisementUrl" target="_blank" rel="nofollow">
-        <img
-          src="../../assets/images/advertisement/ad_970x90.png"
-          alt="Top Ad"
-          class="w-max object-cover md:h-full mx-auto border"
-        />
-      </a>
-    </div>
-  </section>
+    <section class="relative overflow-hidden py-5 border-b">
+        <div class="container mx-auto">
+            <a :href="topAdvertisementUrl" target="_blank" rel="nofollow">
+                <img
+                    src="../../assets/images/advertisement/ad_970x90.png"
+                    alt="Top Ad"
+                    class="w-max object-cover md:h-full mx-auto border"
+                />
+            </a>
+        </div>
+    </section>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      topAdvertisementUrl: "#",
-    };
-  },
-  methods: {},
+    data() {
+        return {
+            topAdvertisementUrl: "#",
+        };
+    },
+    methods: {},
 };
 </script>
