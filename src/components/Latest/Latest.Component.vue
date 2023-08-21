0e897e9d13eb5310f32525f7782b89af27098a98
monzuralam <monzur707@gmail.com>
2023-08-21 08:33:37 +0600

    newsstories component has been added

--- src/components/Latest/Latest.Component.vue
+++ src/components/Latest/Latest.Component.vue
@@ -2,13 +2,15 @@
     <TopAdvertisement />
     <NewsTop cat="Latest" />
     <Advertisement970 src="" href="#" />
+    <NewsStories cat="Latest" />
 </template>
 <script>
 import TopAdvertisement from "../Advertisement/TopAdvertisement.Component.vue";
 import NewsTop from "../News/NewsTop.Component.vue";
 import Advertisement970 from "../Advertisement/Advertisement-970x250.Component.vue";
+import NewsStories from "../News/NewsStories.Component.vue";
 export default {
     name: "Latest",
-    components: { TopAdvertisement, NewsTop, Advertisement970 },
+    components: { TopAdvertisement, NewsTop, Advertisement970, NewsStories },
 };
 </script>
