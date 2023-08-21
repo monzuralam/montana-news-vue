74d2df4ff3cebabf3a8ae4c63674e9ef6a0351b4
monzuralam <monzur707@gmail.com>
2023-08-21 07:12:34 +0600

    Advertisment 970x250 Component has been added

---
+++ src/components/Advertisement/Advertisement-970x250.Component.vue
@@ -0,0 +1,28 @@
+<template>
+    <!-- advertisment section start here -->
+    <section class="relative overflow-hidden pt-2 pb-7 border-b">
+        <div class="container mx-auto">
+            <a :href="link" target="_blank" rel="nofollow">
+                <img
+                    :src="image"
+                    class="w-max object-cover md:h-full mx-auto border"
+                    alt="ads"
+                />
+            </a>
+        </div>
+    </section>
+    <!-- advertisment section stop here -->
+</template>
+<script>
+import adsDefaultImage from "../../assets/images/advertisement/ad_970x250.png";
+export default {
+    name: "Advertisement970",
+    props: ["src", "href"],
+    data() {
+        return {
+            image: this.src ? this.src : adsDefaultImage,
+            link: this.href ? this.href : "#",
+        };
+    },
+};
+</script>
