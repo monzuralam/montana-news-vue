f14b43f9cce68dbb99aa2bb4ef9d646f2c19d907
monzuralam <monzur707@gmail.com>
2023-08-21 07:13:32 +0600

    Advertisement component has been added

--- src/components/Latest/Latest.Component.vue
+++ src/components/Latest/Latest.Component.vue
@@ -1,12 +1,14 @@
 <template>
     <TopAdvertisement />
     <NewsTop cat="Latest" />
+    <Advertisement970 src="" href="#" />
 </template>
 <script>
 import TopAdvertisement from "../Advertisement/TopAdvertisement.Component.vue";
 import NewsTop from "../News/NewsTop.Component.vue";
+import Advertisement970 from "../Advertisement/Advertisement-970x250.Component.vue";
 export default {
     name: "Latest",
-    components: { TopAdvertisement, NewsTop },
+    components: { TopAdvertisement, NewsTop, Advertisement970 },
 };
 </script>
